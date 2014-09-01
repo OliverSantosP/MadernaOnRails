f82146006e8bacc940375b71fb1a0a76f2a37612
Oliver Santos <rails-dev@railsdev-VirtualBox.(none)>
2014-09-01 10:29:52 -0600

    Migrated Abouts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901145959) do
+ActiveRecord::Schema.define(version: 20140901162524) do
+
+  create_table "abouts", force: true do |t|
+    t.text     "message"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "name"
