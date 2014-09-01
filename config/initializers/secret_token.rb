da4d3b7752bf8787fa3cba3f63ae94c54b43f1f9
rails-dev <rails-dev@railsdev-VirtualBox.(none)>
2014-09-01 00:11:10 -0600

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maderna::Application.config.secret_key_base = '8da749168b777945fef6c762bc90e6012b84208659a400ab721f04d697b8101f90b660e7c66eb2ff47b0398751c851d723c94ae75374ca245add7f6db73b723d'
