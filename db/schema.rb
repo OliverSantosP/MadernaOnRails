d2bd943be99fcaeb8b6e83d42aed211ea700e03c
Oliver Santos <rails-dev@railsdev-VirtualBox.(none)>
2014-09-11 12:46:41 -0600

    Renaming Users password column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911182810) do
+ActiveRecord::Schema.define(version: 20140911184410) do
 
   create_table "abouts", force: true do |t|
     t.text     "message"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20140911182810) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
