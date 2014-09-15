0ef18538c328f3c8cf6e9c50a35894f015b90914
Oliver Santos <rails-dev@railsdev-VirtualBox.(none)>
2014-09-15 14:16:45 -0600

    Finish sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Maderna::Application.routes.draw do
 
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   resources :products
 
   resources :pictures
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911184410) do
+ActiveRecord::Schema.define(version: 20140915193403) do
 
   create_table "abouts", force: true do |t|
     t.text     "message"
@@ -62,9 +62,11 @@ ActiveRecord::Schema.define(version: 20140911184410) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
   create_table "welcomes", force: true do |t|
     t.string   "index"
